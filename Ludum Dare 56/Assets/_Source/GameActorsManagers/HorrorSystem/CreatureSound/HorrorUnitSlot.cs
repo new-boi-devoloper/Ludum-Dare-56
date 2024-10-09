e1bd8ae16768abb4eec967625d1efc28996651c2
new-boi-devoloper <dimasikxshow@gmail.com>
2024-10-09 18:39:15 +0300

    refactor: completed screamers
    - added animations to Creature
    -added sounds

--- Ludum Dare 56/Assets/_Source/GameActorsManagers/HorrorSystem/CreatureSound/HorrorUnitSlot.cs
+++ Ludum Dare 56/Assets/_Source/GameActorsManagers/HorrorSystem/CreatureSound/HorrorUnitSlot.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ namespace _Source.GameActorsManagers.HorrorSystem.CreatureSound
 {
     public class HorrorUnitSlot : MonoBehaviour, IHorrorActivate
     {
+        [field: SerializeField] public AnimType UnitAnimType { get; private set; }
         [field: SerializeField] public GameObject HorrorUnitPrefab { get; private set; }
         [field: SerializeField] private float timeForRun;
         [field: SerializeField] private List<GameObject> pathPoints;
@@ -17,12 +19,34 @@ namespace _Source.GameActorsManagers.HorrorSystem.CreatureSound
 
         private void MoveUnit()
         {
-            var copiedUnit = Instantiate(HorrorUnitPrefab);
-            var unitRb = copiedUnit.GetComponent<Rigidbody>();
             var path = pathPoints.ConvertAll(point => point.transform.position).ToArray();
-
+            var copiedUnit = Instantiate(HorrorUnitPrefab, path[0], Quaternion.identity);
+            var unitRb = copiedUnit.GetComponent<Rigidbody>();
+            
+            ChooseAnim(copiedUnit);
+            
             unitRb.DOPath(path, timeForRun, PathType.Linear, PathMode.Full3D)
-                .OnComplete(() => Destroy(copiedUnit)); // Уничтожаем copiedUnit по завершении пути
+                .OnComplete(() => Destroy(copiedUnit));
+        }
+
+        private void ChooseAnim(GameObject copiedUnit)
+        {
+            var unitAnim = copiedUnit.GetComponent<Animator>();
+            if (UnitAnimType == AnimType.Run)
+            {
+                unitAnim.SetBool("Run", true);
+            }
+
+            if (UnitAnimType == AnimType.AngryReaction)
+            {
+                unitAnim.SetBool("AngryReaction", true);
+            }
+        }
+
+        public enum AnimType
+        {
+            Run,
+            AngryReaction
         }
     }
 }
--- Ludum Dare 56/Assets/_Source/GameActorsManagers/HorrorSystem/SoundHorror/HorrorSoundSlot.cs
+++ Ludum Dare 56/Assets/_Source/GameActorsManagers/HorrorSystem/SoundHorror/HorrorSoundSlot.cs
@@ -4,9 +4,11 @@ namespace _Source.GameActorsManagers.HorrorSystem.SoundHorror
 {
     public class HorrorSoundSlot : MonoBehaviour, IHorrorActivate
     {
+        [field: SerializeField] public string horrorSoundToPlay { get; private set; }
+
         public void ActivateHorror()
         {
-            AudioManager.instance.Play("Horror");
+            AudioManager.instance.Play(horrorSoundToPlay);
         }
     }
 }
